122184d6724a8aa8181c85b267691cd7cedf1f24
anacondacode <dimastabig1337@yandex.ru>
2025-02-15 21:35:08 +0300

    add head.php

--- templates/head.php
+++ templates/head.php
@@ -8,4 +8,4 @@
     <title>Портфолио</title>
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
-<body>
+<body class="<?php echo isset($_COOKIE['site_theme']) ? $_COOKIE['site_theme'] : 'day-theme';?>">
