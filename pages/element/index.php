dac6f2ab1bbd7aa92bafb5fc2935b13396ffd6ee
anacondacode <dimastabig1337@yandex.ru>
2025-02-16 13:29:54 +0300

    modify css

---
+++ pages/element/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php $_SERVER["DOCUMENT_ROOT"] ?>/assets/css/element/style.css">
+    <link rel="stylesheet" href="<?php $_SERVER["DOCUMENT_ROOT"] ?>/assets/css/style.css">
+    <title>Элемент</title>
+</head>
+<body>
+<!-- Верхнее меню -->
+<?php include_once($_SERVER["DOCUMENT_ROOT"] . '/templates/top-menu.php'); ?>
+
+<div class="container">
+    <div class="element-symbol">Cs</div>
+    <div class="element-name">Цезий</div>
+    <div class="atomic-number">55</div>
+    <div class="atomic-weight">132.905</div>
+</div>
+</body>
+</html>
