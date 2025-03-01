3b8ec74b869668bb1dd05efb251201d7f00c8bba
RRaymi <robby.ray@yandex.ru>
2025-03-01 15:02:54 +0500

    strings homework

--- index.php
+++ index.php
@@ -1,5 +1,62 @@
-<?php require_once "header.php"; ?>
+<?php require_once "header.php"; 
 
+$about_text = "Родился в Магнитогорске. Закончил МГТУ им.Носова по специальности \"Прикладная информатика\". В данный момент работаю интернет-маркетологом в компании СантехУрал в Челябинске.";
+$review_text = "Нравится большое количество практики";
+
+
+// -- Задание 1 --
+// Окрашивание первого слова в тексте "обо мне" в красный цвет
+$new_about_text = explode(" ", $about_text, 2);
+$new_about_text[0] = "<span style=\"color: red;\">" . $new_about_text[0] . " </span>";
+$about_text = implode($new_about_text);
+
+//Окрашивание каждого второго слова 
+$new_review_text = explode(" ", $review_text);
+
+for ($i = 0; $i < count($new_review_text); $i++) {
+    if ($i % 2 == 0) $new_review_text[$i] = "<span style=\"color: gray;\">" . $new_review_text[$i] . " </span>";
+    else $new_review_text[$i] = "<span style=\"color: blue;\">" . $new_review_text[$i] . " </span>";
+}
+
+$review_text = implode($new_review_text);
+
+// -- Задание 2 + 3 -- Подсчет количества гласных букв на странице
+$vowels = ["а", "е", "ё", "и", "о", "у", "ы", "э", "ю", "я"];
+//$vowels = "аеёиоуыэюя"; // хранилище гласных букв
+$consonants = "";
+$content = trim(strip_tags(file_get_contents("index.php"))); //получение контента и удаление тегов
+$content = mb_strtolower($content);
+
+//подсчет слов
+echo "Количество слов на странице: " . str_word_count($content, 0, "АаБбВвГгДдЕеЁёЖжЗзИиЙйКкЛлМмНнОоПпРрСсТтУуФфХхЦцЧчШшЩщЪъЫыЬьЭэЮюЯя") . "<br>";
+
+
+$content = str_replace(" ", "", $content); //удаление пробелов
+$content = mb_str_split($content);
+
+$count = 0; //счетчик символов 
+
+//Сверяем каждый символ 
+foreach($content as $symb) {
+    foreach ($vowels as $vowel){
+        if ($symb == $vowel) {
+            $count++;
+            continue;
+        } 
+    }
+}
+
+echo "Количество гласных букв на странице $count<br>";
+
+
+// -- Задание 4 --
+
+$currentDay = time();
+$birthday = strtotime("1999-03-10");
+echo "День рождения 10.03.1999 <br>";
+echo "Дней прошло: " . round(($currentDay - $birthday) / (60 * 60 * 24));
+
+?>
     <main>
         <section class="hero">
             <img class="hero-image" src="images/photo.jpg" width="250" height="250">
@@ -8,13 +65,12 @@
                 <div class="hero-container-text">
                     <div class="about-container">
                         <h2 class="about">Обо мне</h2>
-                        <p class="about-text">Родился в Магнитогорске. Закончил МГТУ им.Носова по специальности "Прикладная информатика". В данный момент работаю интернет-маркетологом в компании СантехУрал в Челябинске.
-                        </p>
+                        <p class="about-text"><?= $about_text ?></p>
                     </div>
                     <div class="review-container">
-                        <h2 class="review">Отзыв о 1 уроке</h2>
+                        <h2 class="review">Отзыв об уроках</h2>
                         <p class="review-text">
-                            Пока сказать сложно, но как будто не хватает структурности урока
+                            <?= $review_text ?>
                         </p>
                     </div>
                 </div>
