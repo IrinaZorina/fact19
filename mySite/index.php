60e26ad812fa508ae4bccaa5ca144db9457c2b1e
Maksim Filinkov <msfilinkov@gmail.com>
2025-03-01 18:14:49 +0300

    homework string

--- mySite/index.php
+++ mySite/index.php
@@ -36,9 +36,64 @@
                 }
             </style>';
         }
+        $str_about="Приветствую вас на своем сайте! Этот сайт создан для изучения веб-разработки на курсе ФАКТ Академии.
+                        Я работаю в техподдержке крупной IT компании, и хочу повысить свои компетенции, чтобы попробовать себя в разработке.
+                        В свободное время люблю заниматься спортом, слушать аудио книги и изучать что-то новое!";
+        $str_course='Я выбрал курс от Факт Академии, потому что тут не только изучается веб-разработка, но так же и "1C-Битрикс".
+                        Так же большим преимуществом для меня является подготовка к экзамену для получения сертификата от "1C-Битрикс".
+                        Надеюсь, что на этом курсе я освою все необходимые навыки и знания и успешно сдам экзамен.';
     ?>
 </head>
 
+<?php
+    // текст первой фразы окрашиваем в произвольный цвет
+    $end_str=mb_strpos($str_about,"!");
+    $str_about_clr=mb_substr($str_about,0,mb_strpos($str_about,"!")+1);
+    $str_about=str_replace($str_about_clr,'',$str_about);
+    $str_about_clr= '<span style="color: rgb('.mt_rand(0,255).','.mt_rand(0,255).','.mt_rand(0,255).')">'.$str_about_clr.'</span>';
+    $str_about=$str_about_clr.$str_about;
+    // окрашиваем слова в отзыве в разные цвета
+    $arr_str_course=explode(" ",$str_course);
+    $str_course="";
+    $str_clr1='<span style="color:black">';
+    $str_clr2='<span style="color:darkcyan">';
+    for($i=0;$i<count($arr_str_course);++$i)
+    {   
+        if($i%2==0)
+        {
+            $str_course.=$str_clr1.$arr_str_course[$i].'</span> ';
+        }
+        else
+        {
+            $str_course.=$str_clr2.$arr_str_course[$i].'</span> ';
+        }
+
+    }
+   
+    // считаем количество гласных на странице
+    $str_text=strip_tags($str_about.$str_course);
+    $arr_char=["а", "е", "ё", "и", "о", "у", "ы", "э", "ю", "я","А","Е","Ё","И","О","У","Ы","Э","Ю","Я"];
+    $num_char=0;
+    for($i=0;$i<count($arr_char);++$i)
+    {
+        $num_char+=substr_count($str_text,$arr_char[$i]);
+    }
+    $str_gl='Количество гласных на странице равно '.$num_char;
+    // считаем количество слов на странице
+    $arr_text=explode(" ",$str_text);
+    $num_words=count($arr_text);
+    $str_words='Количество слов на странице '.$num_words;
+    // считаем время от рождения до текущей даты
+    $date_now=date('d-m-Y');
+    $str_date_now="Текущая дата ".$date_now;
+    $str_born="Дата рождения 29-12-1985";
+    $d_now= new DateTime(date('Y-m-d'));
+    $d_born= new DateTime("1985-12-29");
+    $d_diff=date_diff($d_now,$d_born);
+    $str_diff="Разность во времени ".$d_diff->format('%a')." дней";
+
+    ?>
+     <span style="color: rgb(1,2 ,2)"></span>
 <body>
    <?php
         include "pages/header.php";
@@ -52,14 +107,12 @@
                 <h2 class="title-h2">Максим Филинков</h2>
                 <div class="main__text-list">
                     <p class="main__text-item">
-                        Приветствую вас на своем сайте! Этот сайт создан для изучения веб-разработки на курсе ФАКТ Академии.
-                        Я работаю в техподдержке крупной IT компании, и хочу повысить свои компетенции, чтобы попробовать себя в разработке.
-                        В свободное время люблю заниматься спортом, слушать аудио книги и изучать что-то новое!
+                    <?php echo $str_about;
+                    ?>
                     </p>
                     <p class="main__text-item">
-                        Я выбрал курс от Факт Академии, потому что тут не только изучается веб-разработка, но так же и "1C-Битрикс".
-                        Так же большим преимуществом для меня является подготовка к экзамену для получения сертификата от "1C-Битрикс".
-                        Надеюсь, что на этом курсе я освою все необходимые навыки и знания и успешно сдам экзамен.
+                    <?php echo $str_course;
+                    ?>
                     </p>
                 </div>
             </div>
@@ -122,11 +175,17 @@
                 </div>
             </div>
         </section>
+        <p class="text_answer"> 
+            <?php
+                    echo $str_gl.". ".$str_words.'. '.$str_born.'. '.$str_date_now.'. '.$str_diff.'.';
+            ?> 
+        </p>
        
     </main>
     <?php
-    include "pages/footer.php";
-    ?>
+            include "pages/footer.php";
+        ?> 
+    
 </body>
 </html>
 
