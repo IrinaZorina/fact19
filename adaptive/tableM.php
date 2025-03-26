5472afae61735429e25ee7cc8d11399786a1b035
dindinoo <dinashamsitdinova2@gmail.com>
2025-03-27 00:26:02 +0500

    Add files via upload
    Адаптив к сайту

---
+++ adaptive/tableM.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="assets/style/styleM.css">
+</head>
+<body>
+    <div class="container">
+        <div class="itemAs">As</div>
+        <div class="item33">33</div>
+        <div class="item74">74,9216</div>
+        <div class="itemM">Мышьяк</div>
+    </div>
+</body>
+</html>
